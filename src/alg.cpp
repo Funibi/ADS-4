5c20dee68bb5a8366bfc57772c55e5fe470ca7bf
Funibi <139475082+Funibi@users.noreply.github.com>
2024-03-23 15:41:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,32 +1,4 @@
 // Copyright 2021 NNTU-CS
-int cbinsearch(int *arr, int size, int value) {
-  int low = 0;
-  int high = size - 1;
-  int n = 0;
-  while (low <= high) {
-    int mid = low + ((high - low) / 2);
-    if (arr[mid] == value) {
-      n = 1;
-      int net = mid + 1;
-      while ((net < size) && (arr[net] == value)) {
-        net++;
-        n++;
-      }
-      net = mid - 1;
-      while ((net >= 0) && (arr[net] == value)) {
-        net--;
-        n++;
-      }
-      return n;
-    } else if (arr[mid] < value) {
-      low = mid + 1;
-    } else {
-      high = mid - 1;
-    }
-  }
-  return 0;
-}
-
 int countPairs1(int *arr, int len, int value) {
   int n = 0;
   for (int i = 0; i < len - 1; i++) {
@@ -60,9 +32,30 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
   int n = 0;
-  for (int i = 0; i < len - 1; i++) {
-    if (arr[i] <= value) {
-      n += cbinsearch(arr + i + 1, len - i, value - arr[i]);
+  for (int i = 0; i < len; ++i) {
+    int low = i + 1;
+    int high = len - 1;
+    int y = value - arr[i];
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (arr[mid] == y) {
+        n++;
+        int net = mid - 1;
+        while ((net > i) && (arr[mid] == arr[net])) {
+          n++;
+          net--;
+        }
+        net = mid + 1;
+        while ((i < net) && (arr[net] == arr[mid])) {
+          n++;
+          net++;
+        }
+        break;
+      } else if (arr[mid] > y) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
     }
   }
   return n;
